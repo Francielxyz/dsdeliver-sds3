f694a5a62f35d0eeabeb029bc6408bf913f62cc4
Francielxyz <ruanvha15@gmail.com>
2021-05-05 21:18:12 -0300

    Pagination

--- backend/src/main/java/com/devsuperior3/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior3/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,9 @@
-//package com.devsuperior3.dsvendas.repositories;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.devsuperior3.dsvendas.entities.Seller;
-//
-//public interface SaleRepository extends JpaRepository<Seller, Long>{
-//
-//}
+package com.devsuperior3.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior3.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/devsuperior3/dsvendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.devsuperior3.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior3.dsvendas.dto.SaleDto;
+import com.devsuperior3.dsvendas.entities.Sale;
+import com.devsuperior3.dsvendas.repositories.SaleRepository;
+import com.devsuperior3.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDto> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = saleRepository.findAll(pageable);
+		return result.map(x -> new SaleDto(x));
+	}
+	
+}
